26074fb5421711d59fc46cdead1cdf84eb58cd58
itxrock <84038116+itxrock@users.noreply.github.com>
2021-05-16 11:16:53 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,7 +3,7 @@ class config:
     BOT_TOKEN = "1833967721:AAGG6RwnxQG9BJoCLFAKip0Dh294owXb9Yw"
     APP_ID = "4521890"
     API_HASH = "fd6fbee5fa105ff2d66a5a93b1d9f7fc"
-    DATABASE_URL = "postgres://fdciuoyeksdyft:83e7e850d5bc479c2a08a5c70ceb4114cf000829b8fdbeaaf9ba857a5d491f03@ec2-54-87-112-29.compute-1.amazonaws.com:5432/d4a66abpga8ldb"
+    DATABASE_URL = "postgres://xriupsjtjkwssa:533c86859543ea9b0cd28f51df8000d6b52581539988ede02113b9f1893e746b@ec2-54-87-112-29.compute-1.amazonaws.com:5432/d4rkhmdn7i34lu."
     SUDO_USERS = "476790224 505966612 1795049661" # Sepearted by space.
     SUPPORT_CHAT_LINK = "https://t.me/joinchat/RjMQdwLwxCRlMzll
 
